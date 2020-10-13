60c9a58b6047d9efa301be57c1d1684a07d0d44f
Koustav Majumder <koustav516@gmail.com>
2020-10-13 16:56:42 +0530

    Connected with db

--- public/js/app.js
+++ public/js/app.js
@@ -115,8 +115,8 @@ console.log('Hello from resource');
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\koustav\Documents\Pizza_App\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\koustav\Documents\Pizza_App\resources\scss\app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! C:\Users\Koustav's PC\Documents\pizza-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Koustav's PC\Documents\pizza-app\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
--- server.js
+++ server.js
@@ -8,12 +8,31 @@ const initRoutes = require('./routes/web');
 const app = express();
 
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose');
+
+
+const url = 'mongodb://localhost/pizza';
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+  console.log('Database connected');
+}).catch(err =>{
+  console.log('Connection Failed: ', err);
+});
+
 
 app.use(express.static(__dirname + '/public'));
-
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
 
 app.use(initRoutes);
 
