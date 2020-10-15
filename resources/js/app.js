677023696986d39b2b1c749b7545ef751b85b235
Koustav Majumder <koustav516@gmail.com>
2020-10-15 16:20:53 +0530

    Completed add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,26 @@
 import axios from 'axios'
+import Noty from 'noty'
+
 const addToCartBtn = document.querySelectorAll('.add-to-cart');
 let cartCounter = document.querySelector('#item-Counter')
 
 function updateCart(product) {
     axios.post('/update-cart', product).then(res =>{
         cartCounter.innerText = res.data.totalQuantity;
-    });
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item Added to cart',
+            progressBar: false
+        }).show();
+    }).catch(err =>{
+        new Noty({
+            type: 'error',
+            timeout: 2000,
+            text: 'Something went wrong',
+            progressBar: false
+        }).show();
+    })
 }
 
 addToCartBtn.forEach((btn)=>{
